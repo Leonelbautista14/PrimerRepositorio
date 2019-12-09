09c8158071ca696f82bbae982922e3b876b0b94e
PC-03 <PC-03@PC-03>
2019-12-09 17:50:30 -0300

    Primero conexion con GitHub

---
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -0,0 +1,15 @@
+
+package primerrepositorio;
+
+
+public class PrimerRepositorio {
+
+   
+    public static void main(String[] args) {
+  
+        System.out.println(" Primero conexion con GitHub ");
+        
+        
+    }
+    
+}
